3f1612ca3cf1b0408a61ebb6882b48c5eb9121bd
uoftcspost <jasonliuedu@gmail.com>
2021-01-16 13:06:49 -0500

    initial commit

---
+++ roast_bot.py
@@ -0,0 +1,21 @@
+import discord
+import os
+
+TOKEN = "ODAwMDU3NjAyODg0ODk0Nzgw.YAMltA.OJNsiye2ttpEyI1Kum9ynlM6zBM"
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
